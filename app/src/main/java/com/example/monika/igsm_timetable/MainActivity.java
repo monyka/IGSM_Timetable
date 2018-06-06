b1b0599e23f0767e960de73b5b870ca1843b794b
marcelina <marcelinaa.ziarno@gmail.com>
2018-06-06 07:40:22 +0200

    old version

--- app/src/main/java/com/example/monika/igsm_timetable/MainActivity.java
+++ app/src/main/java/com/example/monika/igsm_timetable/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToolbar(){
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Timetable App");
+        getSupportActionBar().setTitle("Timetable");
     }
 
     private void setupListView(){
